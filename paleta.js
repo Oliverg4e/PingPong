959e89807fd0d5102ee5364a08260e894013aff0
Oliverg4e <56056717+Oliverg4e@users.noreply.github.com>
2021-05-11 18:22:21 -0600

    Add files via upload

---
+++ paleta.js
@@ -0,0 +1,23 @@
+class Paleta{
+	constructor(x,y,w,h){
+		this.x=x;
+		this.y=y;
+		this.w=w;
+		this.h=h;
+		
+		
+	}
+
+	display(){
+		rect(this.x,this.y,this.w,this.h);
+
+	}
+
+	
+	
+
+  
+
+	
+
+}
